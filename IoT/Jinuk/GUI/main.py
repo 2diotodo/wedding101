ddb77c662e2967f8c255683c17dd048337a27c64
wlsdnr4729 <wlsdnr4729@naver.com>
2023-01-24 14:01:48 +0900

    IoT : 0124 진욱

---
+++ IoT/Jinuk/GUI/main.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from QT_screens_code.validation import Ui_Form
+
+
+class MyApp(QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.arrow_button_pix = QPixmap("./QT_Assets/proceed.png")
+        self.arrow_icon = QIcon(self.arrow_button_pix)
+        self.ui.pushButton.setIcon(self.arrow_icon)
+        self.ui.pushButton.setIconSize(self.arrow_button_pix.rect().size())
+
+
+app = QApplication()
+app.setApplicationName("Wed101")
+win = MyApp()
+win.showFullScreen()
+
+app.exec()
