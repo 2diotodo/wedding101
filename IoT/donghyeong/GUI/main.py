de9c92dd2f91d0e4e3fd9c6efcfdbb1930dbd6a8
Seonghwan Kim <seonghwan1031@gmail.com>
2023-01-27 02:13:26 +0900

    IoT : [329] 크로마키 배경처리 - cv2 없이 numpy로 처리

--- IoT/donghyeong/GUI/main.py
+++ IoT/donghyeong/GUI/main.py
@@ -30,7 +30,12 @@ class MyThread(QThread):
             self.video_stream = None
             self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
             self.record = False
+            self.background_image = None
+
             self.fps = 60
+            self.width = 640
+            self.height = 480
+
             # mediapipe modules
             self.mp_drawing = mp.solutions.drawing_utils
             self.mp_drawing_styles = mp.solutions.drawing_styles
@@ -39,12 +44,15 @@ class MyThread(QThread):
 
 
     def run(self):
+        # setting the background image
+        self.set_background_image(bg_dir = "./BackgroundImage", bg_name = "flower1.png")
+
         while True:
             ret, self.img = self.cam.read()
 
             if ret:
-                self.resize_image(width = 640, height = 480)
-                self.image_processing(mode = 2, frame_check = 1)
+                self.resize_image()
+                self.image_processing(mode = 3, frame_check = 1)
                 self.printImage()
             else:
                 print("camera is not working")
@@ -54,18 +62,27 @@ class MyThread(QThread):
             self.frame_cnt += 1
             sleep(1/self.fps) # 60 fps
 
-    def resize_image(self, width, height):
+
+    def set_background_image(self, bg_dir = "./BackgroundImage", bg_name = 'flower1.png'):
+        background_path = os.path.join(bg_dir, bg_name)
+        background_image = cv2.imread(background_path)
+        background_image = cv2.resize(background_image, (self.width, self.height))
+        self.background_image = background_image
+
+
+    def resize_image(self):
         image = self.img
         # 상하반전
         image = cv2.flip(image, 0) 
         
         # 1) 가로 모드
         # resizing - default interpolation: linear
-        image = cv2.resize(image, (width, height))
+        image = cv2.resize(image, (self.width, self.height))
 
         # TODO: 2) 세로모드 - rotation + resize
 
         self.img = image
+
 
     def image_processing(self, mode = 2, frame_check = 12):
         '''
@@ -73,97 +90,126 @@ class MyThread(QThread):
         2. Detection - right/left hand, Pose
         3. Chroma key replacement
         '''
-        image = self.img
-        w = round(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = round(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        # 웹캠으로 찰영한 영상을 저장하기
-        # cv2.VideoWriter 객체 생성, 기존에 받아온 속성값 입력
-        out = cv2.VideoWriter('./BackgroundImage/output.avi', self.fourcc, self.fps, (w, h))
 
         # 1. Selfie Segmentation
         if mode == 1:
             self.mediapipe_selfie_segmentation()
 
         # 2. Detection
-        # frame_check : default is 1. chekcing every frame
         elif (mode == 2) and (self.frame_cnt % frame_check == 0):
             self.mediapipe_holistic()
         
         elif (mode == 3) :
             self.chromakey_replacement()
-        
-        out.write(self.img)
+
 
     def mediapipe_holistic(self) :
         with self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-            image = self.img
 
             # Recolor from BGR to RGB
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
 
             # Make Detections
-            results = holistic.process(image)
+            results = holistic.process(self.img)
 
             # Recolor from RGB to BGR
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
 
             # Right hand
-            self.mp_drawing.draw_landmarks(image, 
+            self.mp_drawing.draw_landmarks(self.img, 
                                             results.right_hand_landmarks, 
                                             self.mp_holistic.HAND_CONNECTIONS)
             
             # Left hand
-            self.mp_drawing.draw_landmarks(image, 
+            self.mp_drawing.draw_landmarks(self.img, 
                                             results.left_hand_landmarks, 
                                             self.mp_holistic.HAND_CONNECTIONS)
 
             # # Pose Detection 
-            # self.mp_drawing.draw_landmarks(image, 
+            # self.mp_drawing.draw_landmarks(self.img, 
             #                                results.pose_landmarks, 
             #                                self.mp_holistic.POSE_CONNECTIONS)
-            self.img = image
 
     def mediapipe_selfie_segmentation(self) :
         # For webcam input:
         BG_COLOR = (192, 192, 192) # gray
         with self.mp_selfie_segmentation.SelfieSegmentation(model_selection=1) as selfie_segmentation:
-            bg_dir = "./BackgroundImage"
-            bg_image = os.path.join(bg_dir, f'flower1.png')
-            image = self.img
+            self.img
 
             # Flip the image horizontally for a later selfie-view display, and convert
             # the BGR image to RGB.
-            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+            self.img = cv2.cvtColor(cv2.flip(self.img, 1), cv2.COLOR_BGR2RGB)
+            
             # To improve performance, optionally mark the image as not writeable to
             # pass by reference.
-            image.flags.writeable = False
-            results = selfie_segmentation.process(image)
-
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            self.img.flags.writeable = False
+            self.img.flags.writeable = True
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
 
             # Draw selfie segmentation on the background image.
             # To improve segmentation around boundaries, consider applying a joint
             # bilateral filter to "results.segmentation_mask" with "image".
-            condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+            condition = np.stack((selfie_segmentation.process(self.img).segmentation_mask,) * 3, axis=-1) > 0.1
             
             # The background can be customized.
             #   a) Load an image (with the same width and height of the input image) to
             #      be the background, e.g., bg_image = cv2.imread('/path/to/image/file')
             #   b) Blur the input image by applying image filtering, e.g.,
             #      bg_image = cv2.GaussianBlur(image,(55,55),0)
-            if bg_image is None:
-                bg_image = np.zeros(image.shape, dtype=np.uint8)
-                bg_image[:] = BG_COLOR
-            else:
-                bg_image = cv2.imread(bg_image)
-                bg_image = cv2.resize(bg_image, (np.shape(image)[1], np.shape(image)[0]))
+            if self.background_image is None:
+                self.background_image = np.zeros(self.img.shape, dtype=np.uint8)
+                self.background_image[:] = BG_COLOR
 
-            self.img = np.where(condition, image, bg_image)
+            self.img = np.where(condition, self.img, self.background_image)
+
 
     def chromakey_replacement(self):
-        pass
+        # # check hsv value from image
+        # hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        
+        # # make a mask from hsv values
+        # mask = cv2.inRange(hsv, (50, 150, 0), (70, 255, 255)) # 영상, 최솟값, 최댓값
+        
+        # # utilizing mask to input image
+        # # copyTo(src, mask, dst)
+        # cv2.copyTo(self.background_image, mask, self.img)
+
+        norm_factor = 255
+        b = self.img[:, :, 0] / norm_factor
+        g = self.img[:, :, 1] / norm_factor
+        r = self.img[:, :, 2] / norm_factor
+
+        red_vs_green = (r - g) + .3
+        blue_vs_green = (b - g) + .3
+
+        """
+        Darker pixels would be around 0.
+        In order to ommit removing dark pixels we
+        sum .3 to make small negative numbers to be
+        above 0.
+        """
+
+        red_vs_green = (r - g) + .3
+        blue_vs_green = (b - g) + .3
+
+        """
+        Now pixels below 0. value would have a
+        high probability to be background green
+        pixels.
+        """
+        red_vs_green[red_vs_green < 0] = 0
+        blue_vs_green[blue_vs_green < 0] = 0
+
+        """
+        Combine the red(blue) vs green ratios to
+        set an alpha layer with valid alpha-values.
+        """
+        alpha = (red_vs_green + blue_vs_green) * 255
+        alpha[alpha > 50] = 255
+
+        self.img[alpha == 0] = 0
+
+
 
     def printImage(self):
         imgBGR = self.img
@@ -172,6 +218,7 @@ class MyThread(QThread):
         img = QImage(imgRGB, w, h, byte * w, QImage.Format_RGB888)
         pix_img = QPixmap(img)
         self.mySignal.emit(pix_img)
+
 
 class MyApp(QWidget, Ui_Form):
     userSignal = Signal()
