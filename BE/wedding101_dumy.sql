42147ab8da733385025016d4089cbd8bb5a49f63
김지현 <rla0347@naver.com>
2023-01-27 17:43:52 +0900

    Docs : sql 파일 추가
    Former-commit-id: 69a3b65f2546e2ec4e9ec2d300ed866cb3d3cef7

---
+++ BE/wedding101_dumy.sql
@@ -0,0 +1,7 @@
+USE wedding101_db;
+
+INSERT INTO tbl_user (user_id, user_password, user_name,user_nickname, user_email) VALUES
+("dudwls624", "1234", "권영진", "꿔녕징", "dudwls624@naver.com"),
+("rla0347", "1234", "김지현", "응애", "rla0347@naver.com");
+
+SELECT * FROM tbl_user;
