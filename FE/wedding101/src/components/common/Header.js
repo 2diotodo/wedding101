de1f6405b37a6af9a843ba66e5e4be820c3dffe5
2diotodo <willowbuds62@gmail.com>
2023-01-31 00:20:30 +0900

    front : [103] 메인화면 조정

--- FE/wedding101/src/components/common/Header.js
+++ FE/wedding101/src/components/common/Header.js
@@ -1,30 +1,29 @@
 import './Header.css';
 
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-scroll';
 import { Button } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
-const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
-const onLogout = () =>{
-  // sessionStorage에 userId로 저장되어 있는 아이템을 삭제
-  sessionStorage.removeItem('userId');
-  // 메인으로 이동(새로고침)
-  document.location.href = '/';
-}
+  const onLogout = () => {
+    // sessionStorage에 userId로 저장되어 있는 아이템을 삭제
+    sessionStorage.removeItem('userId');
+    // 메인으로 이동(새로고침)
+    document.location.href = '/';
+  };
 
-useEffect(() => {
-  if(sessionStorage.getItem("name") === null){
-    console.log("isLogin?? ::", isLogin);
-  }else {
-    // sessionStorage에 name이라는 key 값으로 저장된 값이 있다면
-    // 로그인 상태 변경
-    setIsLogin(true);
-    console.log("isLogin?? ::", isLogin);
-  }
-});
+  useEffect(() => {
+    if (sessionStorage.getItem('name') === null) {
+      console.log('isLogin?? ::', isLogin);
+    } else {
+      // sessionStorage에 name이라는 key 값으로 저장된 값이 있다면
+      // 로그인 상태 변경
+      setIsLogin(true);
+      console.log('isLogin?? ::', isLogin);
+    }
+  });
 
   return (
     <div className='header'>
@@ -32,29 +31,29 @@ useEffect(() => {
         <img src='#' alt='wedding101' />
       </div>
       <div className='nav-tabs'>
-        <NavLink labe="About" to='/'>
+        <NavLink labe='About' to='/'>
           <Button variant='text' size='large' color='secondary'>
             About{' '}
           </Button>
         </NavLink>
-        <NavLink label='Invitation' to='/invitation' >
+        <NavLink label='Invitation' to='/invitation'>
           <Button variant='text' size='large' color='secondary'>
             Invitation{' '}
           </Button>
         </NavLink>
-        <NavLink label='Album' to='/album' >
+        <NavLink label='Album' to='/album'>
           <Button variant='text' size='large' color='secondary'>
             {' '}
             Album{' '}
           </Button>
         </NavLink>
-        <NavLink label='Process' to='/user/service01' >
+        <NavLink label='Process' to='/user/service01'>
           <Button variant='text' size='large' color='secondary'>
             Process{' '}
           </Button>
         </NavLink>
         <NavLink label='Review' to='/review'>
-          <Button variant='text' size='large' color='secondary' >
+          <Button variant='text' size='large' color='secondary'>
             Review
           </Button>
         </NavLink>
@@ -64,21 +63,21 @@ useEffect(() => {
           </Button>
         </NavLink>
 
-{isLogin ? (
-        <div>
-          <NavLink label='LogIn' to='/user/mypage'>
-            <Button variant='text' color='secondary'>
-              {sessionStorage.getItem("name")}
-            </Button>
-          </NavLink>
-          <NavLink label='LogOut' to='/user/login'>
+        {isLogin ? (
+          <div>
+            <NavLink label='LogIn' to='/user/mypage'>
+              <Button variant='text' color='secondary'>
+                {sessionStorage.getItem('name')}
+              </Button>
+            </NavLink>
+            <NavLink label='LogOut' to='/user/login'>
               <Button variant='text' color='secondary' onClick={onLogout}>
                 Logout{' '}
               </Button>
             </NavLink>
-        </div>
-          ) :(
-            <div>
+          </div>
+        ) : (
+          <div>
             <NavLink label='LogIn' to='/user/login'>
               <Button variant='text' color='secondary'>
                 LogIn{' '}
@@ -90,10 +89,7 @@ useEffect(() => {
               </Button>
             </NavLink>
           </div>
-          )
-
-}
-
+        )}
       </div>
     </div>
   );
--- FE/wedding101/src/pages/Main/MainIndex.js
+++ FE/wedding101/src/pages/Main/MainIndex.js
@@ -6,19 +6,18 @@ import MainArea02 from '../../pages/Main/MainArea02';
 import MainArea03 from '../../pages/Main/MainArea03';
 import MainArea04 from '../../pages/Main/MainArea04';
 import GoServiceButton from '../../components/common/GoServiceButton';
-import { SectionsContainer, Header, Footer} from 'react-fullpage';
+import { SectionsContainer, Header, Footer } from 'react-fullpage';
 import Headers from '../../components/common/Header';
 import Footers from '../../components/common/Footer';
 
 const MainIndex = () => {
-  const options ={
+  const options = {
     anchors: ['MainArea01', 'MainArea02', 'MainArea03', 'MainArea04'],
-    navigation: false,
-  }
+    navigation: true,
+  };
 
   return (
     <div className='mainindex'>
-
       <Header>
         <Headers />
       </Header>
@@ -26,18 +25,18 @@ const MainIndex = () => {
         <Footers />
       </Footer>
       <SectionsContainer {...options}>
-      <div id='about'>
-        <MainArea01 />
-      </div>
-      <div id='invitation'>
-        <MainArea02 />
-      </div>
-      <div id='album'>
-        <MainArea03 />
-      </div>
-      <div id='process'>
-        <MainArea04 />
-      </div>
+        <div id='about'>
+          <MainArea01 />
+        </div>
+        <div id='invitation'>
+          <MainArea02 />
+        </div>
+        <div id='album'>
+          <MainArea03 />
+        </div>
+        <div id='process'>
+          <MainArea04 />
+        </div>
       </SectionsContainer>
       <div className='goServiceButton'>
         <GoServiceButton />
