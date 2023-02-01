ab8e60c74b8a1443bbe77a6e2b688617dfe3ac2e
Seonghwan Kim <seonghwan1031@gmail.com>
2023-02-01 18:46:49 +0900

    IoT : 촬영 내용 Submit 기능 간단한 구현
    Former-commit-id: b65790b1932b2bf1a9bf3421dbc12e933607cc66

---
+++ IoT/seonghwk/server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './store/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send("Upload Successful!");
+  console.log(req.body)
+  console.log("Received")
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
