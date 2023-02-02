af2ba700d2f6f8c69b43f6d9a626b8dccb2bc7ed
Ubuntu <ubuntu@ip-172-26-1-3.ap-northeast-2.compute.internal>
2023-02-02 23:16:07 +0000

    CI/CD : Modify shell script for deploying spring-server
    Former-commit-id: 287a8edd46d41e12562a7227826f8993e4273812

--- BE/wedding101/del_old_spring_container.sh
+++ BE/wedding101/del_old_spring_container.sh
@@ -1,13 +1,32 @@
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+# 1. for removing '<none>:<none>' docker image
+#    you can check this on the official documentation
+#    https://docs.docker.com/engine/reference/commandline/images/
+DANGLING_DOCKER_IMAGE=$(docker images -f "dangling=true" -q)
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+# 2. for removing 'spring-server' container previously made
+#    'PRE_JAVA_CONTAINER' has all the value which has been made before
+#    'PRE_JAVA_IMAGE', 'PRE_JAVA_TAG' for checking docker images which has "spring-server"
+PRE_JAVA_CONTAINER=$(docker ps -a | grep "java -jar" | awk '{print $1}')
+PRE_JAVA_IMAGE=$(docker images -a | grep "spring-server" | awk '{print $1}')
+PRE_JAVA_TAG=$(docker images -a | grep "spring-server" | awk '{print $2}')
 
-echo " 현재 구동중인 애플리케이션pid: $CURRENT_PID"
+if [-n $PRE_JAR_CONTAINER] ; then
+	echo "Removing pre-container where command including 'java -jar'"
+	docker rm $PRE_JAR_CONTAINER
+else 
+	echo "Great. There's no pre-container where command is 'java -jar'.."
+fi
 
-if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [-n $DANGLING_DOCKER_IMAGE] ; then
+	echo "Removing dangling docker images(<none>:<none>).."
+	docker rmi $DANGLING_DOCKER_IMAGE
 else
-        echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
+	echo "Great. There's no danling docker images(<none>:<none>).."
+fi
+
+if [-n $PRE_JAVA_IMAGE] ; then
+	echo "Removing previous $PRE_JAVA_IMAGE:$PRE_JAVA_TAG.."
+	docker rmi $PRE_JAVA_IMAGE:$PRE_JAVA_TAG
+else
+	echo "Great. There's no previous $PRE_JAVA_IMAGE:$PRE_JAVA_TAG"
 fi
