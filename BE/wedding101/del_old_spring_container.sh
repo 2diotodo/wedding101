cc80ef7059dd0c43a0c669ea71e03ed9c7419655
dhl <lllee.dh@gmail.com>
2023-02-02 23:52:30 +0900

    CI/CD : automation setting test ..
    Former-commit-id: 73bf5d7465524a0413897393cde52a120c96a05d

--- BE/wedding101/del_old_spring_container.sh
+++ BE/wedding101/del_old_spring_container.sh
@@ -1,6 +1,6 @@
 echo "DOCKER_ID Check..."
 
-PRE_DOCKER_ID=$(docker ps | grep 8085->8080 | awk '{print $1}')
+PRE_DOCKER_ID=$(docker ps | grep spring-server | awk '{print $1}')
 PRE_DOCKER_IMAGE=$(docker ps | grep spring-server | awk '{print $2}')
 NONE_DOCKER_IMAGE=$(docker images -f "dangling=true" -q)
 
