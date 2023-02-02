cae709168e6c63fb279f73163b235358cf25c3da
dhl <lllee.dh@gmail.com>
2023-02-02 22:44:05 +0900

    CI/CD : add removing docker rmi function to .sh
    Former-commit-id: 9b2acb16f4d9e5e640abf1555f986fa601e0350c

--- BE/wedding101/del_old_spring_container.sh
+++ BE/wedding101/del_old_spring_container.sh
@@ -1,7 +1,7 @@
 echo "DOCKER_ID Check..."
 
-PRE_DOCKER_ID=$(docker ps | grep spring-server | awk '{print $1}')
-PRE_DOCKER_IMAGE=$(docker images | grep spring-server | awk '{print $3}')
+PRE_DOCKER_ID=$(docker ps | grep server | awk '{print $1}')
+PRE_DOCKER_IMAGE=$(docker images | grep server | awk '{print $3}')
 
 if [ -z $PRE_DOCKER_ID ] ; then
 	echo "Docker is not running"
