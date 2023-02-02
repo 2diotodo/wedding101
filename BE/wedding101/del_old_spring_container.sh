cab43d497d0e922e6a2ef2e15eb45babf4b01d2d
Ubuntu <ubuntu@ip-172-26-1-3.ap-northeast-2.compute.internal>
2023-02-02 06:44:08 +0000

    Fix : Shell script for Jenkins Deployment
    Former-commit-id: 599a530bc79ee320a901ed1a8c5efb06afd03458

--- BE/wedding101/del_old_spring_container.sh
+++ BE/wedding101/del_old_spring_container.sh
@@ -1,15 +1,20 @@
 echo "DOCKER_ID Check..."
 
-PRE_DOCKER_ID=$(docker ps | grep spring-server | awk '{print $1}')
+PRE_DOCKER_ID=$(docker ps | grep service-server | awk '{print $1}')
+PRE_DOCKER_IMAGE=$(docker images | grep service-server | awk '{print $3}')
 
 if [ -z $PRE_DOCKER_ID ] ; then
 	echo "Docker is not running"
 else
 	echo "Running DOCKER_PID: {$PRE_DOCKER_ID}"
+	echo "Stop this DOCKER_PID..."
 	docker stop $PRE_DOCKER_ID
 	sleep 10
+	echo "Done."
+	echo "Do image pruning.."
+	docker image prune -y
+	echo "Done."
 	echo "{$PRE_DOCKER_ID} has been stopped"
-	docker system prune -y
 fi
 
 echo "Deploy Project...."
