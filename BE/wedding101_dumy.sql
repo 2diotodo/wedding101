837b72211378305b246711d36e74bdedb206cdd6
김지현 <rla0347@naver.com>
2023-02-03 11:19:25 +0900

    Fix : change dumy_sql
    Former-commit-id: 188af4d599cbfeb4cb0541624913efc3a11d1a60

--- BE/wedding101_dumy.sql
+++ BE/wedding101_dumy.sql
@@ -1,14 +1,14 @@
 USE wedding101_db;
 
 INSERT INTO tbl_user (user_id, user_password, user_name,user_nickname, user_email) VALUES
-("dudwls624", "1234", "권영진", "꿔녕징", "dudwls624@naver.com"),
-("rla0347", "1234", "김지현", "응애", "rla0347@naver.com");
+("dudwls624", "1234", "kwj", "dhiekd", "dudwls624@naver.com"),
+("rla0347", "1234", "kjh", "sddlkjd", "rla0347@naver.com");
 
 SELECT * FROM tbl_user;
 
 insert into tbl_info (user_seq, wedding_day, wedding_hall_name, groom_name, bride_name, groom_phone_number, bride_phone_number) values
-(1, now(), "더아리엘", "이상현", "권영진", "010-0000-0000", "010-1111-1111"),
-(2, now(), "신라호텔", "김영광", "김지현", "010-1234-1234", "010-9999-1111");
+(1, now(), "theariel", "lsh", "kwj", "010-0000-0000", "010-1111-1111"),
+(2, now(), "weston", "kwg", "kjh", "010-1234-1234", "010-9999-1111");
 
 select * From tbl_info;
 
@@ -18,7 +18,7 @@ insert into tbl_album (info_seq, user_seq, album_name, album_access_id, album_th
 select * from tbl_album;
 
 insert into tbl_media (album_seq, storage_url, media_name, media_relation) values
-(1, "s3_url1", "김나영", "친구"),
-(1, "s3_url1", "나경인", "친구");
+(1, "s3_url1", "kny", "Friend"),
+(1, "s3_url2", "nki", "Friend");
 select * from tbl_media;
 
