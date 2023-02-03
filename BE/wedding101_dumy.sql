bf5a98f3e35c0b654b8e017736e4ed7f5c5dd0dc
김지현 <rla0347@naver.com>
2023-02-03 09:23:28 +0900

    Fix : change dumy_sql
    Former-commit-id: 9a71ce0fcead3de3f6ac96568e9f2468a9269b82

--- BE/wedding101_dumy.sql
+++ BE/wedding101_dumy.sql
@@ -7,10 +7,18 @@ INSERT INTO tbl_user (user_id, user_password, user_name,user_nickname, user_emai
 SELECT * FROM tbl_user;
 
 insert into tbl_info (user_seq, wedding_day, wedding_hall_name, groom_name, bride_name, groom_phone_number, bride_phone_number) values
-(1, now(), "더아리엘", "이상현", "권영진", "010-0000-0000", "010-1111-1111");
+(1, now(), "더아리엘", "이상현", "권영진", "010-0000-0000", "010-1111-1111"),
+(2, now(), "신라호텔", "김영광", "김지현", "010-1234-1234", "010-9999-1111");
 
 select * From tbl_info;
 
 insert into tbl_album (info_seq, user_seq, album_name, album_access_id, album_thanks_url) values
-(1, 1, "추억앨범", "123456789a", "1245364");
+(1, 1, "albumName", "123456789a", "thanks_url1"),
+(2, 2, "HappyDay!", "123456789b", "thanks_url2");
 select * from tbl_album;
+
+insert into tbl_media (album_seq, storage_url, media_name, media_relation) values
+(1, "s3_url1", "김나영", "친구"),
+(1, "s3_url1", "나경인", "친구");
+select * from tbl_media;
+
