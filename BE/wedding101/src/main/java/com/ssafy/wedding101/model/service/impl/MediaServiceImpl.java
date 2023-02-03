02c1f4c8e943dc1192e92db9bba9a0d132511391
김지현 <rla0347@naver.com>
2023-02-03 17:49:23 +0900

    back : [233] 미디어 옵션 조회
    Former-commit-id: a1625b8dd814866852e2f91fee35680a414f14ba

--- BE/wedding101/src/main/java/com/ssafy/wedding101/model/service/impl/MediaServiceImpl.java
+++ BE/wedding101/src/main/java/com/ssafy/wedding101/model/service/impl/MediaServiceImpl.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,8 +32,48 @@ public class MediaServiceImpl implements MediaService {
     }
 
     @Override
-    public List<MediaDto> getMediaList(Long albumSeq, String type, String to, String relation) {
-        return null;
+    public List<MediaDto> getMediaListByOptions(Long albumSeq, int optionNum, Map<String, String> options) {
+        List<MediaDto> result = new ArrayList<>();
+        int type = -1;
+        if(options.containsKey("type")) {
+            if(options.get("type").equals("video")) {
+                type = 1;
+            } else {
+                type = 0;
+            }
+        }
+
+        switch (optionNum) {
+            case 0: // 옵션 안걸려있음
+                result = mediaRepository.findAllByAlbumSeq(albumSeq).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 1: // type
+                result = mediaRepository.findAllByType(albumSeq, type).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 2: // to
+                result = mediaRepository.findAllByTo(albumSeq, options.get("to")).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 3: // type & to
+                result = mediaRepository.findAllByTypeAndTo(albumSeq, type, options.get("to")).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 4: // relation
+                result = mediaRepository.findAllByRelation(albumSeq, options.get("relation")).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 5: // type & relation
+                System.out.println("서비스임플");
+                System.out.println(options.get("relation"));
+                result = mediaRepository.findAllByTypeAndRelation(albumSeq, type, options.get("relation")).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 6: // to & relation
+                result = mediaRepository.findAllByToAndRelation(albumSeq, options.get("to"), options.get("relation")).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            case 7: // all
+                result = mediaRepository.findAllByOptions(albumSeq, type, options.get("to"), options.get("relation")).stream().map(this::toDto).collect(Collectors.toList());
+                break;
+            default:
+
+        }
+        return result;
     }
 
     @Override
