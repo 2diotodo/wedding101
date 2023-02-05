e948094d8f92e8b7b4122c6a70b062159817e1fb
김지현 <rla0347@naver.com>
2023-02-05 22:39:31 +0900

    back : invitation controller 작성-ing
    Former-commit-id: 6e291fa94da8257680161178c32fdd80875ada0d

--- BE/wedding101/src/main/java/com/ssafy/wedding101/controller/InvitationRestController.java
+++ BE/wedding101/src/main/java/com/ssafy/wedding101/controller/InvitationRestController.java
@@ -1,4 +1,29 @@
 package com.ssafy.wedding101.controller;
 
+import com.ssafy.wedding101.model.dto.InvitationDto;
+import com.ssafy.wedding101.model.service.InvitationService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/invitation")
+@CrossOrigin
 public class InvitationRestController {
+    private final InvitationService invitationService;
+
+//    @Operation(summary = "청첩장 생성")
+//    @PostMapping("")
+//    public ResponseEntity<Map<String, Object>> writeInvitation(@RequestBody InvitationDto invitationDto) {
+//        Map<String, Object> result = new HashMap<>();
+//
+//    }
 }
