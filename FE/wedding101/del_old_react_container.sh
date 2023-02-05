3eb5dab1a48d5d9b529eb9327d7509d348220bb0
Ubuntu <ubuntu@ip-172-26-12-109.ap-northeast-2.compute.internal>
2023-02-05 14:58:29 +0000

    CI/CD : Dockerfile PRE_DOCKER_CONTAINER -> print 1
    Former-commit-id: d53df9168c36b6cb41d4dd15750794ec2aa0362f

--- FE/wedding101/del_old_react_container.sh
+++ FE/wedding101/del_old_react_container.sh
@@ -7,17 +7,17 @@ DANGLING_DOCKER_IMAGE=$(docker images -f "dangling=true" -q)
 # 2. for removing 'react' container previously made
 #    'PRE_REACT_CONTAINER' has all the value which has been made before
 #    'PRE_REACT_IMAGE', 'PRE_REACT_TAG' for checking docker images which has "react"
-PRE_REACT_CONTAINER=$(docker ps -a | grep "npm" | awk '{print $2}')
+PRE_REACT_CONTAINER=$(docker ps -a | grep "react" | awk '{print $1}')
 PRE_REACT_IMAGE=$(docker images -a | grep "react" | awk '{print $1}')
 PRE_REACT_TAG=$(docker images -a | grep "react" | awk '{print $2}')
 
 if [ -n $PRE_REACT_CONTAINER ] ; then
-	echo "Removing $PRE_REACT_CONTAINER where command including 'java -jar'"
+	echo "Removing $PRE_REACT_CONTAINER where command including 'npm'"
 	echo 'y' | docker system prune
 	docker stop $PRE_REACT_CONTAINER
 	docker rm $PRE_REACT_CONTAINER
 else 
-	echo "Great. There's no pre-container where command is 'java -jar'.."
+	echo "Great. There's no pre-container where command is 'npm'.."
 fi
 
 if [ -n $DANGLING_DOCKER_IMAGE ] ; then
