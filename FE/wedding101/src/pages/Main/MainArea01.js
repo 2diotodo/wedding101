4af00a35d4ec784b745d0fb7eca8232e750d43d7
2diotodo <2diotodo@gmail.com>
2023-02-07 08:38:06 +0900

    front : [test]
    Former-commit-id: 229e93c28b8c74fc0670810f0832c6f1a72d8df5

--- FE/wedding101/src/pages/Main/MainArea01.js
+++ FE/wedding101/src/pages/Main/MainArea01.js
@@ -2,7 +2,7 @@ import './MainArea01.css';
 
 // import main01_1 from '../../assets/img/mainArea01_1.png';
 // import main01_2 from '../../assets/img/mainArea01_2.png';
-// import main01_3 from '../../assets/img/mainArea01_3.png';
+import main01_3 from '../../assets/img/mainArea01_3.png';
 import main01_4 from '../../assets/img/mainArea01_4.png';
 import main01_5 from '../../assets/img/mainArea01_5.png';
 import main01_6 from '../../assets/img/mainArea01_6.png';
@@ -15,7 +15,7 @@ const MainArea01 = () => {
         <img src={main01_6} alt='main01_6'></img>
         <img src={main01_5} alt='main01_5'></img>
         <img src={main01_4} alt='main01_4'></img>
-        {/* <img src={main01_3} alt='main01_3'></img> */}
+        <img src={main01_3} alt='main01_3'></img>
         {/* <img src={main01_2} alt='main01_2'></img> */}
         {/* <img src={main01_1} alt='main01_1'></img> */}
       </div>
