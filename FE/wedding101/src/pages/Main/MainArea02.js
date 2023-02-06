5fe0a8085ec3bfcaf05d5f8f4facdd7f23640cb4
wlsdnr4729 <wlsdnr4729@naver.com>
2023-02-06 16:19:54 +0900

    FE : [103] Main 화면 작업중
    Former-commit-id: a0897948c86cd1d0d8b92d7369fad2dcf3b89394

--- FE/wedding101/src/pages/Main/MainArea02.js
+++ FE/wedding101/src/pages/Main/MainArea02.js
@@ -1,20 +1,34 @@
 import './MainArea02.css';
 
 import main02_1 from '../../assets/img/mainArea02_1.png';
-import InvitationForm from '../../components/WeddingInvitation/InvitationForm';
 const MainArea02 = () => {
   return (
     <div className='main-area02'>
-      <h1>MainArea02</h1>
-      <p>This is MainArea02!</p>
-      <div className='area'>
-      <div className='side-territory'>
-      <img src={main02_1} alt='main02_1'></img>
+      <div className='container' id='container_p2'>
+        <div className='horizontalLayout' id='HL_p2_01'>
+          <div className='verticalLayout' id='VL_p2_01'>
+            <div className='title_explain'>
+              <div className='title' id='title_p2_01'>Mobile Invitation</div>
+              <div className='explainText' id='explain_p2_01'>설명 설명</div>
+              <div className='explainText' id='explain_p2_02'>Memory Forever</div>
+            </div>
+            <div className='detailLayout'>
+              <button className='detailButton'>자세히 보러가기</button>
+              <div className='horizontalLayout' id='HL_p2_02'>
+                <div className='indexIndicator' id='idx1'>1</div>
+                <div className='indexIndicator' id='idx2'>2</div>
+                <div className='indexIndicator' id='idx3'>3</div>
+                <div className='indexIndicator' id='idx4'>4</div>
+                <div className='indexIndicator' id='idx5'>5</div>
+              </div>
+            </div>
+          </div>
+          <div className='exampleSlider'>
+            <img src={main02_1} alt='main02_1'></img>
+          </div>
+        </div>
       </div>
-      <div className='invitation-form'>
-      <InvitationForm />
-      </div>
-      </div>
+      
     </div>
   );
 };
