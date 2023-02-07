f0df8a52299cf77d6ad635dc1ff5f29000feb4b5
2diotodo <2diotodo@gmail.com>
2023-02-07 17:55:57 +0900

    front : [113] process 개요완료
    Former-commit-id: b5e984882bfbe0bf3c82a93a2ba3234d561a8e16

--- FE/wedding101/src/pages/Album/AlbumCover.js
+++ FE/wedding101/src/pages/Album/AlbumCover.js
@@ -1,12 +1,28 @@
 import './AlbumCover.css';
 
+import { useState } from 'react';
 import UploadMedia from '../../components/common/UploadMedia';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { useNavigate } from 'react-router';
 
 
 function AlbumCover(){
+    const [albumForm, setAlbumForm] = useState({
+        albumSeq: '',
+        infoSeq: '',
+        userSeq: '',
+        albumName: '',
+        albumColor: '',
+        albumPhotoUrl: '',
+        albumAccessId: '',
+        albumThanksUrl: '',
+        albumMediaCnt: '',
+        createdAt: '',
+        updatedAt: '',
+        isValid: '',
 
+    });
+    const userId = sessionStorage.getItem('userId');
     const onAlbumListHandler = () => {
         navigate('/album/list');
     };
@@ -23,11 +39,16 @@ function AlbumCover(){
                     <h1>Album Cover page</h1>
                 </Grid2>
                 <Grid2 lg={6} sm={8}>
-                    <div className='cover-image' onClick={onAlbumListHandler}>
-                        {}님의 앨범
+                    <div className='cover-image' style={{color: albumForm.albumColor}} >
+                        <div className='album-img'>
+                            <img src={albumForm.albumPhotoUrl} alt='img upload' />
+                        </div>
+                        <div className='cover-id' onClick={onAlbumListHandler}>
+                            {userId}님의 앨범
+                        </div>
                     </div>
                     <div className='upload-media'>
-                        <UploadMedia />
+                        <UploadMedia media={albumForm.albumPhotoUrl}/>
                     </div>
                 </Grid2>
                 <Grid2 lg={4} sm={2}>
@@ -35,7 +56,7 @@ function AlbumCover(){
                         <h3>나의 결혼식 날짜</h3>
                         <p>{Date()}</p>
                         <h3>앨범 생성일</h3>
-                        <p>{Date()}</p>
+                        <p>{albumForm.createdAt}</p>
                     </Grid2>
                     <Grid2 lg={4}>
                         <h3 onClick={onClickHandler}>서비스 리뷰하기</h3>
--- FE/wedding101/src/pages/WeddingInvitation/InvitationProcess04.js
+++ FE/wedding101/src/pages/WeddingInvitation/InvitationProcess04.js
@@ -8,6 +8,7 @@ import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import {Button} from '@mui/material/';
 import InvitationForm from '../../components/WeddingInvitation/InvitationForm';
 import ProgressBar from '../../components/common/ProgressBar';
+import axios from 'axios';
 
 const InvitationProcess04 = () => {
     const [invitationForm, setInvitationForm] = useState({
@@ -26,7 +27,7 @@ const InvitationProcess04 = () => {
 
     });
 
-    const submitHandler = () =>{
+    const submitHandler = async (e) =>{
         // sessionStorage to invitationForm
 
         setInvitationForm.photoUrl01(sessionStorage.getItem('photoUrl01'));
@@ -36,8 +37,15 @@ const InvitationProcess04 = () => {
         setInvitationForm.templateEtc(sessionStorage.getItem('textInput03'));
 
         // axios 통신
+        await axios
+        .post('http://localhost:8080/', {
+            data: invitationForm,
+        })
+        .then((res) => {
 
-    }
+        })
+        .catch();
+    };
 
 
     const navigate = useNavigate();
@@ -46,13 +54,15 @@ const InvitationProcess04 = () => {
     };
 
     return(
-        <div>
+        <div className='process04'>
             <Grid2 container spacing={3}>
                  <Grid2 lg={3} sm={2}>
                     <h1>Mobile Invitation</h1>
                 </Grid2>
                 <Grid2 lg={8} sm={10}>
-                <ProgressBar />
+                    <div className='process-main'>
+
+                <ProgressBar steps={3}/>
                     <h2>모바일 청첩장이 생성되었습니다.</h2>
                         <div>
                             <InvitationForm />
@@ -65,6 +75,7 @@ const InvitationProcess04 = () => {
                         <Button variant='contained' type='submit'>완료</Button>
                     </div>
                     </form>    
+                    </div>
                 </Grid2>
             </Grid2>
         </div>
