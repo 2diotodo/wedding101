1250b36975b4f7aecdf0bc7f99c96c0a70740d9d
wlsdnr4729 <wlsdnr4729@naver.com>
2023-02-09 16:03:39 +0900

    FE: [106] progressbar 복구
    Former-commit-id: 8841e88e6b4b59353336f4fb8e21ad73edd6f73b

--- FE/wedding101/src/components/common/ProgressBar.js
+++ FE/wedding101/src/components/common/ProgressBar.js
@@ -2,10 +2,17 @@ import { Step, Stepper, StepLabel } from "@mui/material";
 
 function ProgressBar (props){
 
+    const steps = [
+        'STEP1',
+        'STEP2',
+        'STEP3',
+        'STEP4',
+      ];
+
     return(
         <div>
           <Stepper activeStep={props.steps} alternativeLabel>
-            {props.steps.map((label) => (
+            {steps.map((label) => (
               <Step key={label}>
                 <StepLabel>{label}</StepLabel>
               </Step>
