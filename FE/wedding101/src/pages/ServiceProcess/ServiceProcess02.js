0cf05e550c464f9a52f1b88e6ac074b6aede7f3f
2diotodo <willowbuds62@gmail.com>
2023-02-09 20:45:27 +0900

    front : syntax error 수정
    Former-commit-id: be6882caded3c571d2f50012ea9904c27bcbe225

--- FE/wedding101/src/pages/ServiceProcess/ServiceProcess02.js
+++ FE/wedding101/src/pages/ServiceProcess/ServiceProcess02.js
@@ -3,39 +3,41 @@ import './ServiceProcess02.css';
 import { useNavigate } from 'react-router';
 import ProgressBar from '../../components/common/ProgressBar';
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
-import { Button } from "@mui/material";
-import WeddingInfoForm from '../../components/serviceProcess/WeddingInfoForm';
+import { Button } from '@mui/material';
+import WeddingInfoForm from '../../components/ServiceProcess/WeddingInfoForm';
 
-function ServiceProcess02 (props) {
+function ServiceProcess02(props) {
+  const navigate = useNavigate();
+  const toProcess03 = () => {
+    navigate('/user/service03');
+  };
 
-    const navigate = useNavigate();
-    const toProcess03 = () => {
-      navigate('/user/service03');
-    };
+  return (
+    <div className='service-process02'>
+      <Grid2 container spacing={2}>
+        <Grid2 lg={3} sm={2}>
+          <h1>Service Application</h1>
+        </Grid2>
 
-    return (
-        <div className='service-process02'>
-            <Grid2 container spacing={2}>
-                <Grid2 lg={3} sm={2}>
-                <h1>Service Application</h1>
-                </Grid2>
-
-                <Grid2 lg={9} sm={10}>
-                <ProgressBar steps={['step1', 'step2', 'step3', 'step4']} activeStep={1} />
-                    <h2>개인정보 입력</h2>
-                    <WeddingInfoForm info={props.processForm}/>
-                    <WeddingInfoForm />
-                    <div className='buttons'>
-                    <Button variant='contained' onClick={() => navigate(-1)}>이전</Button>
-                    </div>
-                    <div className='buttons'>
-                    <Button variant='contained' onClick={toProcess03}>다음</Button>
-                    </div>
-                </Grid2>
-
-            </Grid2>
-        </div>
-    )
+        <Grid2 lg={9} sm={10}>
+          <ProgressBar steps={['step1', 'step2', 'step3', 'step4']} activeStep={1} />
+          <h2>개인정보 입력</h2>
+          <WeddingInfoForm info={props.processForm} />
+          <WeddingInfoForm />
+          <div className='buttons'>
+            <Button variant='contained' onClick={() => navigate(-1)}>
+              이전
+            </Button>
+          </div>
+          <div className='buttons'>
+            <Button variant='contained' onClick={toProcess03}>
+              다음
+            </Button>
+          </div>
+        </Grid2>
+      </Grid2>
+    </div>
+  );
 }
 
 export default ServiceProcess02;
