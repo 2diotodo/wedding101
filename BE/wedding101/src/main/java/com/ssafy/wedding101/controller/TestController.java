5f9bbf930118873c271e3f4ea91c711b52535ad6
권영진 <dudwls624@naver.com>
2023-02-09 17:21:35 +0900

    [BE] FFmpeg Util 작성
    Former-commit-id: 9ec89c7dd5b84508df168dbc75a9daf6bace3fad

--- BE/wedding101/src/main/java/com/ssafy/wedding101/controller/TestController.java
+++ BE/wedding101/src/main/java/com/ssafy/wedding101/controller/TestController.java
@@ -1,12 +1,25 @@
 package com.ssafy.wedding101.controller;
 
+import com.ssafy.wedding101.util.FFmpegUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+
+    private final FFmpegUtil fFmpegUtil;
+
     @GetMapping("/log")
     public void log() throws Exception {
         log.fatal("fatal log");
@@ -16,5 +29,16 @@ public class TestController {
         log.debug("debug");
         log.trace("trace");
 
+    }
+
+    @PostMapping("/video/merge")
+    public ResponseEntity<?> mergeWeddingmedia(Map<String, List<String>> listMap) throws Exception {
+
+        fFmpegUtil.downloadImage(listMap.get("imageList"));
+        fFmpegUtil.downloadVideo(listMap.get("videoList"), listMap.get("imageList"));
+        fFmpegUtil.combineImage(listMap.get("imageList"));
+        fFmpegUtil.combineVideo();
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
