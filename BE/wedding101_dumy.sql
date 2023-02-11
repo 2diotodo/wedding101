7b0f0fa12488719ce842e71f78dfcd52602d4ea1
Ubuntu <ubuntu@ip-172-26-14-174.ap-northeast-2.compute.internal>
2023-02-12 02:02:11 +0900

    CI/CD : handling DB-spring language issue 25
    Former-commit-id: d313e8fef1b303071e0fd7b06913547e97a65564

--- BE/wedding101_dumy.sql
+++ BE/wedding101_dumy.sql
@@ -14,7 +14,7 @@ select * From tbl_info;
 
 insert into tbl_album (info_seq, user_seq, album_name, album_access_id, album_thanks_url) values
 (1, 1, "albumName", "123456789a", "https://a101-wedding101-pjt.s3.ap-northeast-2.amazonaws.com/dudwls624/video/00fce54f-555f-457e-a38b-d6f27c44ce4f.mp4"\),
-(2, 2, "HappyDay!", "123456789b", "https://a101-wedding101-pjt.s3.ap-northeast-2.amazonaws.com/dudwls624/video/00fce54f-555f-457e-a38b-d6f27c44ce4f.mp4"\)\;
+(2, 2, "HappyDay!", "123456789b", "https://a101-wedding101-pjt.s3.ap-northeast-2.amazonaws.com/dudwls624/video/00fce54f-555f-457e-a38b-d6f27c44ce4f.mp4"\);
 select * from tbl_album;
 
 -- relation_list = [, family, relatives, friend, colleague, acquaintance]
