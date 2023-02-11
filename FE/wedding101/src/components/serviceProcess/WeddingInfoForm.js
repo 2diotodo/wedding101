ec3c1e0b0017e61bea68dc9e59988307cee55885
wlsdnr4729 <wlsdnr4729@naver.com>
2023-02-11 22:56:14 +0900

    FE: [109] 서비스 프로세스 2 화면은 완료, 값 저장 진행중
    Former-commit-id: ff8ab84b6f6e245900c6a8fc6a60de387455cd7b

--- FE/wedding101/src/components/serviceProcess/WeddingInfoForm.js
+++ FE/wedding101/src/components/serviceProcess/WeddingInfoForm.js
@@ -7,28 +7,7 @@ function WeddingInfoForm(props){
     let gender = '';
     let genderClass = '';
     if (props.bridegroom === 0) {gender = '예비 신랑'; genderClass = 'groom'}
-    else {gender = '예비 신부'; genderClass = 'bride'}
-    const [info, setInfo] = useState({
-        gender: "",
-        name: "",
-        phoneNumber: "",
-        accountBank: "",
-        accountNumber: "",
-        accountName: "",
-        siblingOrder: "",
-        fatherName: "",
-        motherName: "",
-        fatherAlive: "true",
-        motherAlive: "true"
-    });
-
-    const onChangeInfo = (e) => {
-        setInfo({
-            ...info,
-            [e.target.id]: e.target.value,
-        });
-        console.log(e.target.id, e.target.value)
-    }
+    else {gender = '예비 신부'; genderClass = 'bride'}    
 
     const onSubmitHandler = () => {
     }
@@ -39,45 +18,45 @@ function WeddingInfoForm(props){
                     <h3>{gender}</h3>
                     <div className='horizontalLayout'>
                         <h3>이름: </h3>
-                        <TextField className='textInputField' id='name' placeholder='홍길동' onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='name' placeholder='홍길동' onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>전화번호: </h3>
-                        <TextField className='textInputField' id='phoneNumber' placeholder='010-xxxx-xxxx'  onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='phoneNumber' placeholder='010-xxxx-xxxx'  onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>은행: </h3>
-                        <TextField className='textInputField' id='accountBank' placeholder='xx은행'  onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='accountBank' placeholder='xx은행'  onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>계좌번호: </h3>
-                        <TextField className='textInputField' id='accountNumber' onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='accountNumber' onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>계좌주: </h3>
-                        <TextField className='textInputField' id='accountName' placeholder='홍길동' onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='accountName' placeholder='홍길동' onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>형제중: </h3>
-                        <TextField className='textInputField' id='siblingOrder' placeholder='첫째/외동' onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='siblingOrder' placeholder='첫째/외동' onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>아버님 성함: </h3>
-                        <TextField className='textInputField' id='fatherName' placeholder='홍상직' onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='fatherName' placeholder='홍상직' onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>어머님 성함: </h3>
-                        <TextField className='textInputField' id='motherName' placeholder='옥영향' onChange={onChangeInfo}/>
+                        <TextField className='textInputField' id='motherName' placeholder='옥영향' onChange={props.onChangeInfo}/>
                     </div>
                     <div className='horizontalLayout'>
                         <h3>부 생존 여부: </h3>
-                        <select id='fatherAlive' onChange={onChangeInfo}>
+                        <select id='fatherAlive' onChange={props.onChangeInfo}>
                             <option value='true' selected="selected">생존</option>
                             <option value='false'>작고</option>
                             <option value='unknown'>비공개</option>
                         </select>
                     </div>
-                    <div className='horizontalLayout' onChange={onChangeInfo}>
+                    <div className='horizontalLayout' onChange={props.onChangeInfo}>
                         <h3>모 생존 여부: </h3>
                         <select id='motherAlive'>
                             <option value='true' selected="selected">생존</option>
--- FE/wedding101/src/pages/ServiceProcess/ServiceProcess02.js
+++ FE/wedding101/src/pages/ServiceProcess/ServiceProcess02.js
@@ -1,6 +1,7 @@
 import './ServiceProcess02.css';
 
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
 import ProgressBar from '../../components/common/ProgressBar';
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { Button } from '@mui/material';
@@ -8,9 +9,57 @@ import WeddingInfoForm from '../../components/serviceProcess/WeddingInfoForm';
 
 function ServiceProcess02(props) {
   const navigate = useNavigate();
+  const [groomInfo, setGroomInfo] = useState({
+    gender: "",
+    name: "",
+    phoneNumber: "",
+    accountBank: "",
+    accountNumber: "",
+    accountName: "",
+    siblingOrder: "",
+    fatherName: "",
+    motherName: "",
+    fatherAlive: "true",
+    motherAlive: "true"
+  });
+
+  const [brideInfo, setBrideInfo] = useState({
+    gender: "",
+    name: "",
+    phoneNumber: "",
+    accountBank: "",
+    accountNumber: "",
+    accountName: "",
+    siblingOrder: "",
+    fatherName: "",
+    motherName: "",
+    fatherAlive: "true",
+    motherAlive: "true"
+  });
+
+  const onChangeGroomInfo = (e) => {
+    setGroomInfo({
+        ...groomInfo,
+        [e.target.id]: e.target.value,
+    });
+    console.log(e.target.id, e.target.value)
+  }
+
+  const onChangeBrideInfo = (e) => {
+    setBrideInfo({
+        ...brideInfo,
+        [e.target.id]: e.target.value,
+    });
+    console.log(e.target.id, e.target.value)
+  }
+
   const toProcess03 = () => {
-    // navigate('/user/service03');
-    console.log()
+    
+    if (groomInfo.name === "" || brideInfo.name === "") {
+      alert('정보가 입력되지 않았습니다.');
+      return;
+    }
+    else navigate('/user/service03');
   };
 
   return (
@@ -24,8 +73,8 @@ function ServiceProcess02(props) {
           <ProgressBar steps={['step1', 'step2', 'step3', 'step4']} activeStep={1} />
           <h2>개인정보 입력</h2>
           <div className='infoContainer'>
-            <WeddingInfoForm bridegroom={0} name='groomInfo'/>
-            <WeddingInfoForm bridegroom={1} name='brideInfo'/>
+            <WeddingInfoForm bridegroom={0} onChangeInfo={onChangeGroomInfo}/>
+            <WeddingInfoForm bridegroom={1} onChangeInfo={onChangeBrideInfo}/>
           </div>
           <div className='buttons'>
             <Button variant='contained' onClick={() => navigate(-1)}>
