7fe73489cd4efd8c41c1dd1d4a9322d45c781331
김지현 <rla0347@naver.com>
2023-02-14 17:59:32 +0900

    front : 내 정보 -ing
    Former-commit-id: d701b76e80d814937afa4ccb0f1f8507c6a4afad [formerly 74f7a5e196303d3d787c605ae2c535766568c846]
    Former-commit-id: 9448aa2350ac0cf3825356a83261f467efae63aa

--- FE/wedding101/src/components/user/UserModify/ModifyForm.js
+++ FE/wedding101/src/components/user/UserModify/ModifyForm.js
@@ -2,37 +2,64 @@ import axios from 'axios';
 
 import './ModifyForm.css';
 import { useEffect, useState, useNavigate } from 'react';
+import { Paper, TableContainer, Table, TableBody, TableCell, TableHead, TableRow} from '@mui/material';
 
+function UserTable(props) {
+    return (
+        <TableContainer component={Paper}>
+            <Table className="user-table" sx={{ minWidth: 550 }} aria-label="simple table">
+                <TableBody>
+                    <TableRow>
+                        <TableCell variant="head" align="center">아이디</TableCell>
+                        <TableCell>{props.data.userId}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell variant="head" align="center">이름</TableCell>
+                        <TableCell>{props.data.userName}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell variant="head" align="center">닉네임</TableCell>
+                        <TableCell>{props.data.userNickname}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell variant="head" align="center">이메일</TableCell>
+                        <TableCell>{props.data.userEmail}</TableCell>
+                    </TableRow>
+
+                </TableBody>
+            </Table>
+
+        </TableContainer>
+    )
+}
 
 function ModifyForm() {
-    // const navigate = useNavigate();
-    // const toModifyUserPage = ()=> {
-    //     navigate('/user/modify')
-    // }
-
-    // const request = axios.create({
-    //     baseURL : "http://i8a101.p.ssafy.io:8085"
-    // })
-    // const api = {
-        
-    // }
     const [user, setUser] = useState([]);
     useEffect(() =>  {
         // 컴포넌트 불러올때  getUser() 실행
         getUser();
     }, []);
+    const {userId, userName, userNickname, userEmail} = user;
     async function getUser() {
-        await axios
-            .get(`http://wedding101.shop/api/user?userSeq=2`)
-            .then((res) => {
+        await axios ({
+            method : "GET",
+            url : "http://wedding101.shop/api/" + 'user',
+            headers : {
+                "Authorization" : "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJybGEwMzQ3IiwidXNlclNlcSI6MiwiaWF0IjoxNjc2MzYzNzc4LCJleHAiOjE2NzYzNjU1Nzh9.aLlbi3j9WiFemhNw5iBiOwg_Xqv-c9jUZSsaUJjK70M"
+            } 
+        }).then((res) => {
                 setUser(res.data.data);
-                console.log(res);
-            })
-            console.log(user);
+                console.log(user)
+        })
+        console.log(user);
     }
+
+
     return (
         <div>
-            <h3>이거용</h3>
+            <h3>{userId}</h3>
+            <UserTable data={user}/>
+            <h3>{userEmail}</h3>
         </div>
     );
 }
