7752ee11cdfa75234b24702b9d1a278e1687e256
2diotodo <2diotodo@gmail.com>
2023-02-14 12:58:12 +0900

    front : [120] syntax 수정
    Former-commit-id: 284b17142302de3bd5f6e20c707cf36a5d0abea4

--- FE/wedding101/src/pages/Album/AlbumDeleted.js
+++ FE/wedding101/src/pages/Album/AlbumDeleted.js
@@ -71,7 +71,7 @@ const AlbumDeleted = () => {
     <div className='album-list'>
       <Grid2 container spacing={3}>
         <Grid2 lg={3} sm={3}>
-          <h1>Album List</h1>
+          <h1>Album Deleted</h1>
           <br />
           <FormControl fullWidth>
             <InputLabel id='sort-label'>정렬조건</InputLabel>
@@ -82,8 +82,8 @@ const AlbumDeleted = () => {
               label='Sort'
               onChange={orderHandler}
             >
-              <MenuItem value={'createdAt'}>날짜</MenuItem>
-              <MenuItem value={'createdAtRev'}>날짜역순</MenuItem>
+              <MenuItem value={'createdAt'}>최근순</MenuItem>
+              <MenuItem value={'createdAtRev'}>오래된순</MenuItem>
               <MenuItem value={'mediaName'}>이름</MenuItem>
             </Select>
           </FormControl>
