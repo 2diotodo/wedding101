9defdf56ca19e61b103f2e4a253f9dacd5431d19
dhl <lllee.dh@gmail.com>
2023-02-15 10:08:56 +0900

    FE : 주석처리
    Former-commit-id: 316b0bcac905adbac193542de0ded7d7658d80e0

--- FE/wedding101/src/components/user/UserLogin/LoginForm.js
+++ FE/wedding101/src/components/user/UserLogin/LoginForm.js
@@ -28,7 +28,8 @@ function LoginForm() {
     // 버튼만 누르면 리프레시되는 것 막기
     event.preventDefault();
     console.log('here');
-    
+
+
     await axios
     .post('http://wedding101.shop/api/user/login', {
       userId: inputId,
@@ -37,24 +38,25 @@ function LoginForm() {
     )
     .then((res) => {
         console.log(res);
-        console.log('res.data.userId :', res.data.data.userId);
-        console.log('res.data.msg :', res.data.message);
-        if (res.data.data.userId === undefined) {
-          // id 불일치, msg= '가입되지 않은 id입니다.'
-          console.log('=========================', res.data.message);
-          alert('가입되지 않은 id입니다.');
-        } else if (res.data.data.userId === null) {
-          // id는 있지만, pw 다른 경우 userId = null
-          console.log('====================', '입력하신 비밀번호가 일치하지 않습니다.');
-          alert('입력하신 비밀번호가 일치하지 않습니다.');
-        } else if (res.data.data.userId === inputId) {
-          // id, pw 모두 일치
-          console.log('=====================', '로그인 성공');
-          // sessionStorage에 id를 userId라는 key 값으로 저장
-          sessionStorage.setItem('userId', inputId);
-          sessionStorage.setItem('userNickname', res.data.data.userNickname)
-          sessionStorage.setItem('name', res.data.data.userName);
-        }
+
+        // console.log('res.data.userId :', res.data.data.userId);
+        // console.log('res.data.msg :', res.data.message);
+        // if (res.data.data.userId === undefined) {
+        //   // id 불일치, msg= '가입되지 않은 id입니다.'
+        //   console.log('=========================', res.data.message);
+        //   alert('가입되지 않은 id입니다.');
+        // } else if (res.data.data.userId === null) {
+        //   // id는 있지만, pw 다른 경우 userId = null
+        //   console.log('====================', '입력하신 비밀번호가 일치하지 않습니다.');
+        //   alert('입력하신 비밀번호가 일치하지 않습니다.');
+        // } else if (res.data.data.userId === inputId) {
+        //   // id, pw 모두 일치
+        //   console.log('=====================', '로그인 성공');
+        //   // sessionStorage에 id를 userId라는 key 값으로 저장
+        //   sessionStorage.setItem('userId', inputId);
+        //   sessionStorage.setItem('userNickname', res.data.data.userNickname)
+        //   sessionStorage.setItem('name', res.data.data.userName);
+        // }
         //작업 완료되면 페이지 이동
         document.location.href = '/';
       })
