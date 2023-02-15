2740383500a4d982b87b3d63654051c6e9c70485
2diotodo <2diotodo@gmail.com>
2023-02-15 15:58:12 +0900

    front : [121] 통합본 신청
    Former-commit-id: 411d2fe7796850f73088bc19a62eb8550b7dc8a6 [formerly 978d44643ebe042840459f6872c27e5cfe72b92b]
    Former-commit-id: c9ac0fb636bf0dfeef689e6db82aa9b2e9289656

--- FE/wedding101/src/components/common/Footer.js
+++ FE/wedding101/src/components/common/Footer.js
@@ -4,7 +4,13 @@ function Footer() {
   return (
     <div className='footer'>
       <footer>
-        <h2>this is footer</h2>
+        <h3>A101</h3>
+        <div>팀장: 김성환</div>
+        <div>팀원: 권영진</div>
+        <div>김지현</div>
+        <div>류제엽</div>
+        <div>이동형</div>
+        <div>이진욱</div>
       </footer>
     </div>
   );
